71b513b573fecd29f88249d65ecac5654ad3931e
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-20 11:21:00 -0400

    loaded DB

--- questionsdbconnect.rb
+++ questionsdbconnect.rb
@@ -10,3 +10,18 @@ class QuestionsDatabase < sqlite3::Database
         self.results_as_hash = true
     end
 end
+
+class Question
+    class self.all
+        data= QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map do {|ele|Question.new(ele)}
+
+    end
+    def initialize(arg)
+        id=arg['id']
+        title=arg['title']
+        body=arg['body']
+        author_id=arg['author_id']
+    end
+end
+table1=QuestionsDatabase.new
